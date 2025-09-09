c23abb5d0b870c1f2e7320857f1fed01cf10cdc0
Alexander <corey158@yandex.ru>
2025-09-09 23:26:57 +0300

    Added division into encounters

--- text_fsm.py
+++ text_fsm.py
@@ -1,5 +1,5 @@
 import os, textfsm, tabulate, pprint
-
+import datetime
 
 template_dps='fsm_dps.textfsm'
 file='CombatLog-2025-08-11_1950.txt'
@@ -15,9 +15,33 @@ def parser(template,combatlog):
     return result
 
 
+def encounter_calc():
+
+    last_time = datetime.datetime.strptime('00:00:00', '%H:%M:%S')
+    delta = datetime.timedelta(0)
+    
+    while True:
+        time_str = yield delta
+        
+        if time_str is None:
+            continue
+            
+        current_time = datetime.datetime.strptime(time_str, '%H:%M:%S')
+        delta = current_time - last_time
+        last_time = current_time
+
+
+
 def sum_damage_by_attacker_target(data):
     damage_stats = {}
     player_name = 'Pvenab'
+    encounter_end_sec = 15
+
+    calc = encounter_calc()
+    next(calc)
+
+    enc_number = 0
+
 
     for entry in data:
         if entry[1] == 'Your' or entry[1] == 'You':
@@ -36,18 +60,25 @@ def sum_damage_by_attacker_target(data):
         damage = int(entry[5])
         damage_type = entry[6]
         crit = entry[7]
+
+        enc_delta = calc.send(time)
+        # print(enc_delta.seconds)
+        
+        if enc_delta.seconds > encounter_end_sec:
+            enc_number = enc_number + 1
         # print(f"{time} | {attacker} | {hit} | {target} | {combo} | {damage} | {damage_type} | {crit}")
 
-
         # key = (attacker, target, combo, crit)
-        key = (attacker, target, combo)
-
+        key = (enc_number, attacker, target)
         if key in damage_stats:
             damage_stats[key] += damage
         else:
             damage_stats[key] = damage
 
+
     return damage_stats
+
+
 
 
 if __name__ == '__main__':
